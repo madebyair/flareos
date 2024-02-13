f09d3507bcf90fdd572efc245505d8720bbd3f46
Oskar Nizioł <oskarniziol@gmail.com>
2024-02-13 07:36:03 +0100

    Button element

--- src/components/setup/welcome/WelcomeSetup.tsx
+++ src/components/setup/welcome/WelcomeSetup.tsx
@@ -1,8 +1,13 @@
+import Button from "../../../elements/Button.tsx";
+
 const WelcomeSetup = () => {
     return (
         <div className="">
             <h1 className="text-5xl dark:text-white">Welcome!</h1>
-            <h2 className="text-zinc-700 dark:text-zinc-300 mt-4 w-72">Welcome to Airos! Before we begin, let's connect to the network and log in to your air account."</h2>
+            <h2 className="text-zinc-700 dark:text-zinc-300 mt-4 w-72">Welcome to Airos! Before we begin, let's connect to the network and log in to your air account.</h2>
+            <Button label="Continue" submit={() => {
+                console.log("Button works")
+            }} />
         </div>
     )
 }
