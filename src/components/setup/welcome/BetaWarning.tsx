5ef1f7488df0fbf2ae78bd284f367937e2230ffe
Oskar Nizioł <oskarniziol@gmail.com>
2024-02-14 21:59:53 +0100

    Add BetaWarning to setup

---
+++ src/components/setup/welcome/BetaWarning.tsx
@@ -0,0 +1,62 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import Button from "../../../elements/Button.tsx";
+import { useTranslation } from "react-i18next";
+
+export default function BetaWarning() {
+    const { t } = useTranslation()
+
+
+    return (
+        <>
+            <Transition appear show as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={() => console.log("idks")}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black/25" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-zinc-800 p-6 text-left align-middle shadow-xl transition-all">
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="text-lg font-medium leading-6 text-white"
+                                    >
+                                        {t("You are now using beta of airos")}
+                                    </Dialog.Title>
+                                    <div className="mt-2">
+                                        <p className="text-sm text-gray-500">
+                                            {t("You are currently using the beta version of airos. This is associated with a large number of bugs and system instability. Currently, no stable version of the system has been released. When we release the stable release, you will need to download an update package approximately ~2GB in size. We do not currently recommend using this system on your main computer.")}
+                                            <br /><b className="text-white">{t("BAD THINGS MAY HAPPEN IF YOU DON'T READ THIS")}</b>
+                                        </p>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <Button label={t("Continue")} submit={() => console.log("id")}></Button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </>
+    )
+}
--- src/components/setup/welcome/WelcomeSetup.tsx
+++ src/components/setup/welcome/WelcomeSetup.tsx
@@ -4,10 +4,12 @@ import { useState } from "react";
 import { useAtomState } from "@zedux/react";
 import { colorSchemeState } from "../../../state/themeState.ts";
 import { useTranslation } from "react-i18next";
+import BetaWarning from "./BetaWarning.tsx";
 
 const WelcomeSetup = () => {
     const [enabled, setEnabled] = useState(false)
     const [, setThemeScheme] = useAtomState(colorSchemeState)
+    const [betaWarning, setBetaWarning] = useState(false)
 
     const { t } = useTranslation()
 
@@ -21,21 +23,26 @@ const WelcomeSetup = () => {
     }
 
     return (
-        <div className="relative h-full">
-            <h1 className="text-5xl dark:text-white">{t("Welcome!")}</h1>
-            <h2 className="text-zinc-700 dark:text-zinc-300 mt-4 w-72">
-                {t("Welcome to Airos! Before we begin, let's connect to the network and log in to your air account.")}
-            </h2>
-            <div className="absolute bottom-0 w-full">
-                <div className="flex">
-                    <Toggle onChange={trigger} enabled={enabled} setEnabled={setEnabled} />
-                    <span className="text-zinc-700 dark:text-zinc-300 ml-3 mt-[4px]">{t("Dark mode")}</span>
-                </div>
-                <div className="absolute top-0 right-0">
-                    <Button label={t("Continue")} submit={() => console.log("Button works")} />
+        <>
+            {betaWarning &&
+                <BetaWarning />
+            }
+            <div className="relative h-full">
+                <h1 className="text-5xl dark:text-white">{t("Welcome!")}</h1>
+                <h2 className="text-zinc-700 dark:text-zinc-300 mt-4 w-72">
+                    {t("Welcome to Airos! Before we begin, let's connect to the network and log in to your air account.")}
+                </h2>
+                <div className="absolute bottom-0 w-full">
+                    <div className="flex">
+                        <Toggle onChange={trigger} enabled={enabled} setEnabled={setEnabled} />
+                        <span className="text-zinc-700 dark:text-zinc-300 ml-3 mt-[4px]">{t("Dark mode")}</span>
+                    </div>
+                    <div className="absolute top-0 right-0">
+                        <Button label={t("Continue")} submit={() => setBetaWarning(true)} />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
