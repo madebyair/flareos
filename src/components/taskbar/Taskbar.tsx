a71b71e251f443cec75416dfb6eb53459e475a5b
Oskar Nizioł <oskarniziol@gmail.com>
2024-02-20 08:28:15 +0100

    taskbar light mode support

--- src/components/taskbar/Taskbar.tsx
+++ src/components/taskbar/Taskbar.tsx
@@ -1,6 +1,6 @@
 const Taskbar = () => {
     return (
-        <div className="w-screen bg-black h-10 z-50">
+        <div className="w-screen bg-zinc-300 dark:bg-zinc-950 h-10 z-50 flex">
             <h1>I</h1>
         </div>
     )
