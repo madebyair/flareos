eb81641b3995f5f4d22edfc0da0e8e05c6c2d35e
Oskar Nizioł <oskarniziol@gmail.com>
2024-02-20 08:14:08 +0100

    simple desktop component

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import AuthCurrentUser from "./AuthCurrentUser.tsx";
 import { get } from "../../store_manager.ts";
 import User from "../../types/user.ts";
+import Desktop from "../desktop/Desktop.tsx";
 
 const Auth = () => {
     let currentUser = 0;
@@ -19,14 +20,12 @@ const Auth = () => {
     return (
         <>
             {inDesktop &&
-                <div className="z-0 w-screen h-screen bg-black">
-                    <h1>Desktop</h1>
-                </div>
+                <Desktop />
             }
-            <div className={`w-screen h-screen auth-bg select-none absolute top-0 ${
-                inDesktop && "fadeout"
-            }`}>
-                <div className="w-screen h-screen backdrop-blur-md flex">
+            <div className={`w-screen h-screen auth-bg select-none absolute top-0`}>
+                <div className={`w-screen h-screen backdrop-blur-md flex ${
+                    inDesktop && "fadeout"
+                }`}>
                     <div className="m-auto">
                         {users.length > 0 ? (
                             <AuthCurrentUser key={users[currentUser]["uuid"]} user={users[currentUser]}
---
+++ src/components/desktop/Desktop.tsx
@@ -0,0 +1,9 @@
+const Desktop = () => {
+    return (
+        <div className="desktop-bg w-screen h-screen bg-black">
+            <></>
+        </div>
+    )
+}
+
+export default Desktop
