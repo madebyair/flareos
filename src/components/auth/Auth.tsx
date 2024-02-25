d8b20852efea07214129b0cda4b508ebed8d6b9b
Oskar Nizioł <oskarniziol@gmail.com>
2024-02-25 11:15:13 +0100

    set user state in auth

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,22 +1,25 @@
-import { useEffect, useState } from "react";
-import AuthCurrentUser from "./AuthCurrentUser.tsx";
-import { get } from "../../store_manager.ts";
-import User from "../../types/user.ts";
-import Desktop from "../desktop/Desktop.tsx";
+import { useEffect, useState } from "react"
+import AuthCurrentUser from "./AuthCurrentUser.tsx"
+import { get } from "../../store_manager.ts"
+import User from "../../types/user.ts"
+import Desktop from "../desktop/Desktop.tsx"
+import { useAtomState } from "@zedux/react"
+import { userState } from "../../state/currentUserState.ts"
 
 const Auth = () => {
-    let currentUser = 0;
-    const [users, setUsers] = useState<Array<User>>([]);
+    const currentUser = 0
+    const [users, setUsers] = useState<Array<User>>([])
     const [inDesktop, setInDesktop] = useState(false)
     const [hide, setHide] = useState(false)
+    const [, setUser] = useAtomState(userState)
 
     useEffect(() => {
         get("users").then((users) => {
             if (Array.isArray(users)) {
-                setUsers(users);
+                setUsers(users)
             }
-        });
-    }, []);
+        })
+    }, [])
 
     return (
         <>
@@ -24,24 +27,25 @@ const Auth = () => {
                 <Desktop />
             }
             {!hide &&
-                <div className={`w-screen h-screen auth-bg select-none absolute top-0`}>
+                <div className={"w-screen h-screen auth-bg select-none absolute top-0"}>
                     <div className={`w-screen h-screen backdrop-blur-md flex ${
                         inDesktop && "fadeout"
                     }`}>
                         <div className="m-auto">
                             {users.length > 0 ? (
                                 <AuthCurrentUser key={users[currentUser]["uuid"]} user={users[currentUser]}
-                                                 onDesktop={() => {
-                                                     setInDesktop(true)
-                                                     setTimeout(() => setHide(true), 800)
-                                                 }}/>
+                                    onDesktop={() => {
+                                        setInDesktop(true)
+                                        setUser(users[currentUser])
+                                        setTimeout(() => setHide(true), 800)
+                                    }}/>
                             ) : <span>Loading</span>}
                         </div>
                     </div>
                 </div>
             }
         </>
-    );
-};
+    )
+}
 
-export default Auth;
+export default Auth
