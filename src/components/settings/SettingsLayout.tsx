eeb6fd9c16fb89f36211da61c549855e27aed3d0
Oskar Nizioł <oskarniziol@gmail.com>
2024-02-27 08:12:08 +0100

    add component prop to SettingsSidebarItem

--- src/components/settings/SettingsLayout.tsx
+++ src/components/settings/SettingsLayout.tsx
@@ -1,16 +1,23 @@
 import SettingsSidebarItem from "./SettingsSidebarItem.tsx"
 import "../../assets/css/App.css"
 import { faDownload, faPalette, faWifi } from "@fortawesome/free-solid-svg-icons"
+import { settingsComponent } from "./settingsState.tsx"
+import { useAtomState } from "@zedux/react"
 
 const SettingsLayout = () => {
+    const [component] = useAtomState(settingsComponent)
+
     return (
         <div className="w-screen h-screen bg-slate-300 flex select-none">
             <div className="w-1/2 p-4">
                 <div className="mt-2 mb-2">
-                    <SettingsSidebarItem name="Connections" icon={faWifi} description="Wifi ・ Bluetooth"/>
-                    <SettingsSidebarItem name="Personalization" icon={faPalette} description="Dark mode ・ Wallpaper"/>
-                    <SettingsSidebarItem name="Updates" icon={faDownload} description="Download and Install ・ Labs"/>
+                    <SettingsSidebarItem name="Connections" icon={faWifi} description="Wifi ・ Bluetooth" component={<></>}/>
+                    <SettingsSidebarItem name="Personalization" icon={faPalette} description="Dark mode ・ Wallpaper" component={<></>}/>
+                    <SettingsSidebarItem name="Updates" icon={faDownload} description="Download and Install ・ Labs" component={<></>}/>
                 </div>
+            </div>
+            <div className="w-1/2 h-screen mt-2 mb-2">
+                {component}
             </div>
         </div>
     )
--- src/components/settings/SettingsSidebarItem.tsx
+++ src/components/settings/SettingsSidebarItem.tsx
@@ -1,16 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core"
+import { useAtomState } from "@zedux/react"
+import { settingsComponent } from "./settingsState.tsx"
 
 type SettingsSidebarItemType = {
     name: string,
     icon: IconDefinition,
-    description: string
+    description: string,
+    component: JSX.Element
 }
 
-const SettingsSidebarItem = ({name, icon, description} : SettingsSidebarItemType) => {
+const SettingsSidebarItem = ({name, icon, description, component} : SettingsSidebarItemType) => {
+    const [, setComponent] = useAtomState(settingsComponent)
+
     return (
         <div className="bg-slate-400/30 hover:bg-slate-400/80 transition duration-300
-         w-full h-28 rounded-xl mt-4 mb-4">
+         w-full h-28 rounded-xl mt-4 mb-4" onClick={() => {
+             setComponent(component)
+        }}>
             <div className="flex w-full h-14">
                 <div className="flex mt-auto mb-auto mx-6">
                     <div className="flex">
