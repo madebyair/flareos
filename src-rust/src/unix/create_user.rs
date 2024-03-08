47c7b77727529d7024fadfc2ae9642782e50f1bd
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-08 17:33:27 +0100

    fix double users with this same names

--- src-rust/src/unix/create_user.rs
+++ src-rust/src/unix/create_user.rs
@@ -1,7 +1,14 @@
 use std::process::Command;
 
 #[tauri::command]
-pub fn create_user(name: String, uuid: String, password: String) {
+pub fn create_user(name: String, uuid: String, password: String) -> String {
+    let exists = user_exists(name.clone());
+
+    if (exists) {
+        println!("Frontend requested creation of new user, but it's arleady used, so shell will create with new name: {}", name);
+        return create_user(name.clone() + "1", uuid.clone(), password.clone());
+    }
+
     Command::new("sh")
              .arg("-c")
              .arg(format!("userdel {} 2>/dev/null || true", name))
@@ -29,4 +36,18 @@ pub fn create_user(name: String, uuid: String, password: String) {
               .arg(format!("echo '{}:{}' | chpasswd", name, password))
               .output()
               .unwrap_or_else(|_| panic!("Main thread crashed by chpasswd`"));
+
+   name
+}
+
+fn user_exists(name: String) -> bool {
+    let command = Command::new("sh")
+        .arg("-c")
+        .arg(format!("getent passwd {}", name))
+        .output()
+        .unwrap_or_else(|_| panic!("Main thread crashed by getent"));
+
+    let exit = command.status.code().unwrap_or(1);
+
+    exit == 2
 }
