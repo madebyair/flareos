a2ee62e0d751bcb179bc3c5b15b8288c35e106c6
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-20 19:18:17 +0100

    store sidebar

--- src/components/store/StoreComponent.tsx
+++ src/components/store/StoreComponent.tsx
@@ -3,6 +3,8 @@ import { emit, listen } from "@tauri-apps/api/event"
 import User, { defaultUser } from "../../types/user.ts"
 import { useTranslation } from "react-i18next"
 import "../../assets/css/App.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome } from "@fortawesome/free-solid-svg-icons"
 
 const StoreComponent = () => {
     const [user, setUser] = useState<User>(defaultUser)
@@ -31,10 +33,14 @@ const StoreComponent = () => {
 
     return (
         <div className={user.theme}>
-            <div className="w-screen h-screen bg-slate-200 dark:bg-black dark:text-white select-none">
+            <div className="w-screen h-screen bg-slate-200 dark:bg-black dark:text-white select-none flex">
                 <div className="w-1/3 h-screen">
                     <div className="w-full h-24 flex">
                         <img src="/src/assets/images/airstorewhite.png" alt=""/>
+                    </div>
+                    <div className="mt-8 w-10/12 text-xl font-bold ml-4">
+                        <FontAwesomeIcon icon={faHome} />
+                        <span className="ml-4">Home</span>
                     </div>
                 </div>
             </div>
