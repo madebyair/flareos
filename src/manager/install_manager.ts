ed3ba38ca9545f71a83c3c5e4f5a3de04e3f39ae
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-22 17:31:23 +0100

    make http request, when installed

--- src/manager/install_manager.ts
+++ src/manager/install_manager.ts
@@ -1,5 +1,6 @@
 import { storeApp } from "../types/storeApp.ts"
 import { invoke } from "@tauri-apps/api/core"
+import axios from "axios"
 
 export const installing: string[] = []
 
@@ -9,6 +10,8 @@ export function isInstalling(uuid: string): boolean {
 
 export async function install(app: storeApp, user: string) {
     if (isInstalling(app.uuid)) return
+
+    await axios.get("https://api.made-by-air.com/store/download?uuid=" + app.uuid)
 
     installing.push(app.uuid)
     console.log("Installing app", user)
