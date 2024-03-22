3dfe3d65bb739631f7d2b34076726e0e6a23504d
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-22 22:17:03 +0100

    update layout of start

--- src/components/start/StartComponent.tsx
+++ src/components/start/StartComponent.tsx
@@ -99,23 +99,25 @@ const StartComponent = () => {
     return (
         <div className={user?.theme}>
             <div className="start bg-slate-200/95 dark:bg-zinc-950/95 w-screen h-screen rounded-xl">
-                <div>
-                    <div className="mx-4 pt-2">
-                        {apps.map(app => (
-                            <div key={app.name} onClick={() => run(app.exec)} className="mx-8 mt-2 hover:bg-slate-300 dark:hover:bg-zinc-800 transition duration-300 rounded-md w-20 h-24">
-                                <div className="flex w-20">
-                                    <img src={app.icon} alt="" width="60px" height="60px" className="m-auto rounded-md"/>
-                                </div>
-                                <div className="text-center w-20 dark:text-white">
-                                    {app.name}
-                                </div>
+                <div className="mx-4 pt-2 grid grid-cols-4 gap-4 justify-items-center"> {/* Modified */}
+                    {apps.map(app => (
+                        <div key={app.name} onClick={() => run(app.exec)} className="hover:bg-slate-300 dark:hover:bg-zinc-800 transition duration-300 rounded-md w-20 h-24 flex flex-col items-center justify-center"> {/* Modified */}
+                            <div className="flex justify-center w-full">
+                                <img src={app.icon} alt="" width="60px" height="60px" className="rounded-md"/>
                             </div>
-                        ))}
-                    </div>
+                            <div className="text-center w-full dark:text-white">
+                                {app.name}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
     )
+
+
+
+
 }
 
 export default StartComponent
