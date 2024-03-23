88f6955658a3bec309dfd667f63cae4fabd4e767
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-23 11:40:20 +0100

    fix store manager type

--- src/manager/store_manager.ts
+++ src/manager/store_manager.ts
@@ -6,7 +6,7 @@ export function get(key: string) {
     return store.get(key)
 }
 
-export function set(key: string, value : Array<never>|string) {
+export function set(key: string, value : unknown) {
     return store.set(key, value).then(() => {
         save()
     })
