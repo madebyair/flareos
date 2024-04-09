d265466cecb07b6f620eaf7349bc886138be36c3
Oskar Nizioł <oskarniziol@gmail.com>
2024-04-09 20:56:12 +0200

    move logos to icons (ik bruh moment)

--- src/components/settings/information/InformationHeader.tsx
+++ src/components/settings/information/InformationHeader.tsx
@@ -1,6 +1,5 @@
 import packageJson from "../../../../package.json"
 import { useTranslation } from "react-i18next"
-
 const InformationHeader = () => {
     const [ t ] = useTranslation()
 
@@ -8,8 +7,8 @@ const InformationHeader = () => {
         <div className="w-full h-32 rounded-md bg-slate-400/30 dark:bg-zinc-900 flex">
             <div className="h-32 w-32 flex">
                 <div className="m-auto">
-                    <img src="/src/assets/images/airos-white.webp" alt="" className="hidden dark:block "/>
-                    <img src="/src/assets/images/airos-black.webp" alt="" className="dark:hidden "/>
+                    <img src="icons://airos-white.webp" alt="" className="hidden dark:block" draggable={false}/>
+                    <img src="icons://airos-black.webp" alt="" className="dark:hidden" draggable={false} />
                 </div>
             </div>
             <div className="mt-auto mb-auto ml-4">
