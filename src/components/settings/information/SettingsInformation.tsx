7d094a70c163e1a8e32300133a684b3898776734
Oskar Nizioł <oskarniziol@gmail.com>
2024-04-09 16:26:35 +0200

    information buttons

--- src/components/settings/information/SettingsInformation.tsx
+++ src/components/settings/information/SettingsInformation.tsx
@@ -1,9 +1,17 @@
 import InformationHeader from "./InformationHeader.tsx"
+import SettingsSelection from "../SettingsSelection.tsx"
+import { useTranslation } from "react-i18next"
 
 const SettingsInformation = () => {
+    const [ t ] = useTranslation()
     return (
         <div className="w-full mt-6">
             <InformationHeader />
+            <div className="mt-4">
+                <SettingsSelection name={t("Software information")} onClick={() => {}} />
+                <SettingsSelection name={t("Hardware information")} onClick={() => {}} />
+                <SettingsSelection name={t("Updates")} onClick={() => {}} />
+            </div>
         </div>
     )
 }
