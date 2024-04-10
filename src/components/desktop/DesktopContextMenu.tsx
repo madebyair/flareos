95ae46963176cea38fef6e2cae5b8714f76de002
Oskar Nizioł <oskarniziol@gmail.com>
2024-04-10 14:27:49 +0200

    desktop context menu design

--- src/components/desktop/DesktopContextMenu.tsx
+++ src/components/desktop/DesktopContextMenu.tsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faImage, faPlus } from "@fortawesome/free-solid-svg-icons"
+import { useTranslation } from "react-i18next"
+
 type ContextType = {
     x: number,
     y: number,
@@ -5,14 +9,37 @@ type ContextType = {
 }
 
 const DesktopContextMenu = ({context} : {context: ContextType}) => {
+    const [ t ] = useTranslation()
+
     return (
         <>
             {context.displayed &&
-                <div className="bg-slate-300 w-32 h-48 absolute" style={{
+                <div className="bg-slate-300 dark:bg-zinc-950 w-56 h-28 absolute rounded-md dark:text-white" style={{
                     top: context.y,
                     left: context.x
                 }}>
-                    Add widget
+                    <div className="m-4">
+                        <div className="w-full h-10 hover:bg-slate-400/50 dark:hover:bg-zinc-900/95 rounded-md transition duration-300 flex">
+                            <div className="h-10 w-10 flex">
+                                <div className="m-auto">
+                                    <FontAwesomeIcon icon={faPlus} />
+                                </div>
+                            </div>
+                            <div className="my-auto">
+                                {t("Add widget")}
+                            </div>
+                        </div>
+                        <div className="w-full h-10 hover:bg-slate-400/50 dark:hover:bg-zinc-900/95 rounded-md transition duration-300 flex">
+                            <div className="h-10 w-10 flex">
+                                <div className="m-auto">
+                                    <FontAwesomeIcon icon={faImage} />
+                                </div>
+                            </div>
+                            <div className="my-auto">
+                                {t("Change wallpaper")}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             }
         </>
