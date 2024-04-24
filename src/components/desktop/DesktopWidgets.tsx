7583efa5a52566f06fa98d46d245e2dbfab3bebb
Oskar Nizioł <oskarniziol@gmail.com>
2024-04-24 09:11:36 +0200

    save widgets location

--- src/components/desktop/DesktopWidgets.tsx
+++ src/components/desktop/DesktopWidgets.tsx
@@ -1,31 +1,16 @@
 import { useAtomState } from "@zedux/react"
 import { userState } from "../../state/currentUserState.ts"
-import Draggable from "react-draggable"
-import ClockWidget from "../widgets/clock/ClockWidget.tsx"
+import Widget from "./Widget.tsx"
 
 const DesktopWidgets = () => {
     const [user] = useAtomState(userState)
 
-    return (
-        <div className="w-screen h-screen absolute top-0 z-50">
-            {user.widgets.map((w) => {
-                let component: JSX.Element = <ClockWidget />
 
-                if (w.component == "clockwidget") {
-                    component = <ClockWidget />
-                }
+    return (
+        <div className="w-screen h-screen absolute top-0 z-10">
+            {user.widgets.map((k) => {
                 return (
-                    <Draggable
-                        key={w.name + w.x}
-                        position={{x: w.x, y: w.y}}
-                        onStop={(e) => {
-                            console.log(e)
-                            console.log(window.innerHeight)
-                        }}
-                        scale={1}
-                    >
-                        <div>{component}</div>
-                    </Draggable>
+                    <Widget w={k} key={k.component} />
                 )
             })}
         </div>
---
+++ src/components/desktop/Widget.tsx
@@ -0,0 +1,54 @@
+import { Widget as WidgetType } from "../../types/widget.ts"
+import ClockWidget from "../widgets/clock/ClockWidget.tsx"
+import Draggable from "react-draggable"
+import { get, set } from "../../manager/store_manager.ts"
+import User from "../../types/user.ts"
+import { useAtomState } from "@zedux/react"
+import { userState } from "../../state/currentUserState.ts"
+import { useState } from "react"
+
+const Widget = ({ w }: { w: WidgetType }) => {
+    const [, setUser] = useAtomState(userState)
+    const [x, setX] = useState(w.x)
+    const [y, setY] = useState(w.y)
+
+    // @ts-ignore
+    const handle = (event, dragElement: { x: number; y: number }) => {
+        setX(dragElement.x)
+        setY(dragElement.y)
+        setUser((prevUser: User) => {
+            get("users").then((r) => {
+                const cur: unknown = r
+
+                if (Array.isArray(cur)) {
+                    const indexToUpdate = cur.findIndex((key: User) => key.uuid === prevUser.uuid)
+                    if (indexToUpdate !== -1) {
+                        const widgetIndex = cur[indexToUpdate].widgets.findIndex((key: WidgetType) => key.component === w.component)
+                        cur[indexToUpdate].widgets[widgetIndex].x = dragElement.x
+                        cur[indexToUpdate].widgets[widgetIndex].y = dragElement.y
+                        set("users", cur)
+                    }
+                }
+            })
+            return { ...prevUser }
+        })
+    }
+
+    let component: JSX.Element = <ClockWidget />
+
+    if (w.component === "clockwidget") {
+        component = <ClockWidget />
+    }
+    return (
+        <Draggable
+            key={w.name + w.x}
+            position={{ x: x, y: y }}
+            scale={1}
+            onStop={handle}
+        >
+            <div>{component}</div>
+        </Draggable>
+    )
+}
+
+export default Widget
