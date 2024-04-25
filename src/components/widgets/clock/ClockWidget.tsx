f8f20c7a5931bb7ea9cabe59264a1a991ad3f29b
Oskar Nizioł <oskarniziol@gmail.com>
2024-04-25 22:50:37 +0200

    clock widget

--- src/components/widgets/clock/ClockWidget.tsx
+++ src/components/widgets/clock/ClockWidget.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
+
 const ClockWidget = () => {
+    const [, i18n] = useTranslation()
+
+    const [time, setTime] = useState(() => {
+        const now = new Date()
+
+        const timeFormat = new Intl.DateTimeFormat(undefined, {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: i18n.language === "en"
+        })
+
+        return timeFormat.format(now)
+    })
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(() => {
+                const now = new Date()
+                const timeFormat = new Intl.DateTimeFormat(undefined, {
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: i18n.language === "en"
+                })
+
+                return timeFormat.format(now)
+            })
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, [])
+
     return (
         <div className="bg-black rounded-md w-48 h-16 flex">
-            <h1 className="m-auto text-xl font-bold dark:text-white">Now (TM)</h1>
+            <h1 className="m-auto text-xl font-bold dark:text-white">{time}</h1>
         </div>
     )
 }
