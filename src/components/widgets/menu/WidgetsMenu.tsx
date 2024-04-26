ada59280e4af5e9671eb37b090d9dbbe13cb94ab
Oskar Nizioł <oskarniziol@gmail.com>
2024-04-26 13:41:46 +0200

    close widgets menu after widget selection

--- src/components/widgets/menu/WidgetsMenu.tsx
+++ src/components/widgets/menu/WidgetsMenu.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next"
 import { emit, listen } from "@tauri-apps/api/event"
 import "../../../i18n.ts"
 import "../../../assets/css/App.css"
-import Button from "../../../elements/Button.tsx"
+import { getCurrent } from "@tauri-apps/api/window"
 
 const WidgetsMenu = () => {
     const [user, setUser] = useState<User>(defaultUser)
@@ -35,17 +35,19 @@ const WidgetsMenu = () => {
         <div className={user?.theme}>
             <div className="w-screen h-screen p-8 bg-slate-200 dark:bg-black dark:text-white select-none">
                 <h1 className="text-3xl font-bold">{t("Widgets")}</h1>
-                { /* TODO widget list here */}
-                <Button submit={() => emit("widget-add", {
-                    name: "Clock",
-                    component: "clockwidget",
-                    x: 10,
-                    y: 20,
-                    default: "clock"
-                })} label="Test add"/>
-                <div className="w-full relative flex">
+                <div className="w-full relative flex mt-6">
                     <div
-                        className="absolute left-0 w-5/12 p-2 hover:bg-slate-400/50 dark:hover:bg-zinc-900/95 rounded-md transition duration-300">
+                        className="absolute left-0 w-5/12 p-2 hover:bg-slate-400/50 dark:hover:bg-zinc-900/95 rounded-md transition duration-300" onClick={() => {
+                            emit("widget-add", {
+                                name: "Clock",
+                                component: "clockwidget",
+                                x: 10,
+                                y: 20,
+                                default: "clock"
+                            })
+
+                            getCurrent().close()
+                        }}>
                         <div
                             className="bg-gradient-to-r from-purple-900/70 via-purple-700/70 to-purple-500/70 h-24 rounded-xl flex">
                             <div className="bg-slate-300 dark:bg-zinc-950 rounded-md w-32 h-12 flex m-auto">
