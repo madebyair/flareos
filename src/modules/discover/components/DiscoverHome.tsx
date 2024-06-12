e6d9fec95206b7e7b7c2d7da2c2d629ec01cbf48
Oskar Nizioł <oskarniziol@gmail.com>
2024-06-12 22:05:42 +0200

    fix design

--- src/modules/discover/components/DiscoverHome.tsx
+++ src/modules/discover/components/DiscoverHome.tsx
@@ -6,7 +6,7 @@ const DiscoverHome = () => {
         <div className="w-full h-full p-4">
             <h1 className="text-2xl font-bold">{t("Discover your computer in your way")}</h1>
             <div className="bg-gradient-to-br from-zinc-700 to-indigo-600 w-full h-52 mt-6 rounded-md relative">
-                <div className="absolute right-2 bottom-2 p-4 bg-zinc-900/70 rounded-md">
+                <div className="absolute right-2 bottom-2 p-4 bg-zinc-900/70 rounded-md text-xl font-bold">
                     Something will be here. Soon
                 </div>
             </div>
