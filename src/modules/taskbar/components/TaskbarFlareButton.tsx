0282c820810d29e62a554f364501883efc030544
Oskar Nizioł <oskarniziol@gmail.com>
2024-06-29 11:33:25 +0200

    fix start button

--- src/modules/taskbar/components/TaskbarFlareButton.tsx
+++ src/modules/taskbar/components/TaskbarFlareButton.tsx
@@ -5,6 +5,7 @@ import { userState } from "../../../state/currentUserState.ts"
 
 const TaskbarFlareButton = () => {
     const [isStartDisplayed, setIsStartDisplayed] = useState(false)
+    const [hover, setHover] = useState(false)
     const [user] = useAtomState(userState)
 
     useEffect(() => {
@@ -15,8 +16,8 @@ const TaskbarFlareButton = () => {
 
     useEffect(() => {
         emit("start-display-event", {
-            "user": user,
-            "current": isStartDisplayed
+            user: user,
+            current: isStartDisplayed
         })
     }, [isStartDisplayed])
 
@@ -27,9 +28,14 @@ const TaskbarFlareButton = () => {
     return (
         <div className="mx-6 flex h-8 my-auto z-10">
             <div
-                className="flex mx-2 rounded-md hover:bg-zinc-400 dark:hover:bg-zinc-800 transition duration-300 h-8 w-8 flex"
-                onClick={() => onStartClick()}>
-                <div className="border-4 w-5 h-5 rounded-md m-auto border-gray-700 hover:border-gray-100 transition"></div>
+                className="flex mx-2 rounded-md hover:bg-zinc-400 dark:hover:bg-zinc-800 transition duration-300 h-8 w-8"
+                onClick={onStartClick}
+                onMouseOver={() => setHover(true)}
+                onMouseOut={() => setHover(false)}
+            >
+                <div
+                    className={`border-4 w-5 h-5 rounded-md m-auto transition ${hover ? "border-gray-100" : "border-gray-700"}`}
+                ></div>
             </div>
         </div>
     )
