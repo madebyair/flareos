09ce32177f868627b9176305a8640d8977121ed3
Oskar Nizioł <oskarniziol@gmail.com>
2024-06-29 18:37:55 +0200

    files hover and double clicks

--- src/components/files/explorer/FileEntry.tsx
+++ src/components/files/explorer/FileEntry.tsx
@@ -1,32 +1,57 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faFile, faFolder } from "@fortawesome/free-solid-svg-icons"
-import { useAtomState } from "@zedux/react"
-import { currentDirState } from "../filesState.tsx"
-import { push } from "../files_history.ts"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFile, faFolder } from "@fortawesome/free-solid-svg-icons";
+import { useAtomState } from "@zedux/react";
+import { currentActiveState, currentDirState } from "../filesState.tsx";
+import { push } from "../files_history.ts";
+import { useEffect, useState } from "react"
 
 type Entry = {
     path: string,
     is_dir: boolean
-}
+};
 
-const FileEntry = ({path, is_dir} : Entry) => {
-    const [, setDirectory] = useAtomState(currentDirState)
+const FileEntry = ({ path, is_dir }: Entry) => {
+    const [, setDirectory] = useAtomState(currentDirState);
+    const [active, setActive] = useAtomState(currentActiveState)
+    const [bg, setBg] = useState("")
+
+    useEffect(() => {
+        if (active == path) {
+            setBg("bg-blue-500/20")
+        } else {
+            setBg("bg-slate-400/30 dark:bg-zinc-950/20")
+        }
+    }, [active]);
+
+    const handleSingleClick = () => {
+        setActive(path);
+    };
+
+    const handleDoubleClick = () => {
+        if (is_dir) {
+            setDirectory(path);
+            push(path);
+        }
+    };
 
     return (
-        <div className="flex h-12 bg-slate-400/30 dark:bg-zinc-950/20 mt-2 mb-2 rounded-md" onClick={() => {
-            if (is_dir) {
-                setDirectory(path)
-                push(path)
-            }
-        }}>
-            <div className="mt-auto mb-auto ml-2">
-                <FontAwesomeIcon icon={is_dir ? faFolder : faFile} />
-            </div>
-            <div className="mt-auto mb-auto ml-2">
-                {path.split("/").pop()}
+        <div className="transition">
+            <div
+                className={`flex h-12  mt-2 mb-2 rounded-md ${bg}`}
+                onClick={handleSingleClick}
+                onDoubleClick={handleDoubleClick}
+                onMouseOver={() => setBg("bg-blue-400/10")}
+                onMouseOut={() => setBg("bg-slate-400/30 dark:bg-zinc-950/20")}
+            >
+                <div className="mt-auto mb-auto ml-2">
+                    <FontAwesomeIcon icon={is_dir ? faFolder : faFile} />
+                </div>
+                <div className="mt-auto mb-auto ml-2">
+                    {path.split("/").pop()}
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default FileEntry
+export default FileEntry;
--- src/components/files/filesState.tsx
+++ src/components/files/filesState.tsx
@@ -1,3 +1,4 @@
 import { atom } from "@zedux/react"
 
 export const currentDirState = atom<string>("currentDir", "/home")
+export const currentActiveState = atom<string>("currentActive", "")
